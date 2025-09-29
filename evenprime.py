fa7ebdc01e95d148bc3ad6148bf752f4cfb210c1
Kavyatotad1406 <totadkavya@gmail.com>
2025-09-29 20:30:34 +0530

    save for master

--- evenprime.py
+++ evenprime.py
@@ -3,3 +3,22 @@ if num % 2 == 0:
     print(f"{num} is Even")
 else:
     print(f"{num} is Odd")
+
+ # Function to check prime
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):  # check divisibility up to sqrt(n)
+        if n % i == 0:
+            return False
+    return True
+
+
+# Main program
+num = int(input("Enter a number: "))
+
+if is_prime(num):
+    print(num, "is a prime number.")
+else:
+    print(num, "is not a prime number.")
+
